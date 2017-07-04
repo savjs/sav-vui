0dfeae7ce1ae561b831fca9a8d89ffa179a90c77
jetiny <86287344@qq.com>
2017-07-04 20:48:20 +0800

    pass?

--- demo/admin/views/Element/ElementIndex.vue
+++ demo/admin/views/Element/ElementIndex.vue
@@ -3,7 +3,7 @@
     ElementIndex
     <api></api>
     <example 
-      comment="comment: 样例的详细说明" 
+      comment="comment: 样例的详细说明0" 
       title="title: 样例的标题" 
       component="BtnModify" 
       content="var x = 10"></example>
