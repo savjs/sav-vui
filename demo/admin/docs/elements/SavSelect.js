5227c54a9afc0247409f2981663397de95b1d0d0
xlon <873685744@qq.com>
2017-07-18 13:30:52 +0800

    set

---
+++ demo/admin/docs/elements/SavSelect.js
@@ -0,0 +1,12 @@
+export let SavSelect = {
+  shortName: '下拉框',
+  mixins: ['size', 'color', 'defaultSlot'],
+  examples: [
+    {
+      name: 'SelectBasic',
+      title: `基础样式`,
+      comment: `修饰器`,
+      file: '@$examples/select/SelectBasic.vue$@'
+    }
+  ]
+}
---
+++ demo/admin/docs/examples/select/SelectBasic.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="as-control">
+      <sav-color-modify v-model="color"></sav-color-modify>
+      <sav-size-modify v-model="size"></sav-size-modify>
+    </div>
+    <div>
+      <sav-select v-model="value" :options="options"></sav-select>
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+    data (){
+      return {
+        color: false,
+        size: false,
+        value: 2,
+        options: [
+          {text: '数学', value: 1},
+          {text: '语文', value: 2},
+          {text: '英语', value: 3}
+        ]
+      }
+    }
+  }
+</script>
