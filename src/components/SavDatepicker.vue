b83eb0bb9e08b2230ba7a0dd27f8d60437e595e1
ricky <ricky@hfjy.com>
2017-07-25 16:16:20 +0800

    fix datepicker

--- src/components/SavDatepicker.vue
+++ src/components/SavDatepicker.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="datetime-picker" :style="{ width: width }">
+    <div class="sav-datetime-picker" :style="{ width: width }">
         <input
             type="text"
             name="{{name}}"
@@ -50,8 +50,8 @@
         data () {
             return {
                 show: false,
-                days: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-                months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                days: ['日', '一', '二', '三', '四', '五', '六'],
+                months: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
                 date: [],
                 now: new Date()
             };
